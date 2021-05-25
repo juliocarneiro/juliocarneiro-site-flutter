1b116efdb747d3d29d6690669f5e235043021fd8
juliocarneiro <juliocarneiro@gmail.com>
2021-05-25 20:26:33 -0300

    feat: initial config and header

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:juliocarneiro/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'juliocarneiro.com.br',
+      theme: ThemeData(
+        textTheme: GoogleFonts.ubuntuTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF25262A),
+      ),
+      home: Home(),
+    );
+  }
+}
